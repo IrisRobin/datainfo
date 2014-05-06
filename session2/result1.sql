c96a72abd32f9181292c79384fae7da5457102f6
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 14:39:17 +0200

    exersice 2.1 en start 2.2

---
+++ session2/result1.sql
@@ -0,0 +1,21 @@
+Insert into Problem1 values("a",false, "meerdere voertuigen kunnen een bepaalde capaciteit hebben, dus weet je nog niet over welk voertuig je het hebt");
+
+Insert into Problem1 values ("b",true, "een voertuig heeft een bepaalde capaciteit");
+
+INSERT INTO Problem1 VALUES ("c", false, "op 1 factuur kunnen meerdere pakketjes vermeld worden");
+
+INSERT INTO Problem1 VALUES ("d", true, "met behulp van 4, 7, 8 is er maar 1 combinatie mogelijk");
+
+INSERT INTO Problem1 VALUES ("e", true, "met behulp van 5 en 4 is er 1 combi mogelijk");
+
+INSERT INTO Problem1 VALUES ("f", true, "aannemende dat B maar voor 1 voertuig gerechtigd is, is het waar. anders niet");
+
+INSERT INTO Problem1 VALUES ("g", false, "een voertuig mag door meerdere bezorgers bestuurd worden");
+
+INSERT INTO Problem1 VALUES ("h", false, "een voertuig kan meerdere bestuurders hebben");
+
+INSERT INTO Problem1 VALUES ("i", true, "bij 1 voertuig hoort 1 capaciteit. De rest hangt hier niet mee samen");
+
+INSERT INTO Problem1 VALUES ("j", false, "zie h");
+
+
