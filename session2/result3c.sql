840c33f1a9017ada0ef093ad134811a5acdcd36a
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 15:28:36 +0200

    second rewriting query. The shunting rule is used

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT p.name FROM Person p, Writes w WHERE w.pid = p.pid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = w.mid);
+SELECT DISTINCT p.name FROM Person p, Writes w, Directs d WHERE w.pid = p.pid AND d.mid != w.mid);
