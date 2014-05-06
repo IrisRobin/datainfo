d73d9c9bebc9a05967011767556914746837d1ec
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 15:22:46 +0200

    third step writing query

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,1 +1,1 @@
-SELECT p.name FROM person p WHERE EXISTS( SELECT * FROM Writes W WHERE w.pid = p.pid AND 'w wrote a movie without director';
+SELECT p.name FROM Person p WHERE EXISTS( SELECT * FROM Writes w WHERE w.pid = p.pid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = w.mid);
