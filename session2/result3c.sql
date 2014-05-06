f9afb04965e9115386ad869c0cdbbaa6cea3584a
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 16:15:09 +0200

    poging 2. (vorige commit poging 3 moet poging 2 zijn). first step rewriting query. The shunting rule is used

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,2 +1,2 @@
-SELECT p.name FROM person p WHERE EXISTS(SELECT * FROM Writes w WHERE w.pid = p.pid AND EXISTS(SELECT * FROM Movie m WHERE m.mid = w.mid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = m.mid)));
+SELECT p.name FROM person p, Writes w WHERE w.pid = p.pid AND EXISTS(SELECT * FROM Movie m WHERE m.mid = w.mid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = m.mid)));
 
