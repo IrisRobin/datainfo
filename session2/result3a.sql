a96def9a1b588cdd450dd3325f4499290a6e17e1
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 15:02:48 +0200

    exercise3a and first rewriting exercise3b. The shunting rule is used.

---
+++ session2/result3a.sql
@@ -0,0 +1,1 @@
+select m.name, m.year from movie M where m.rating >= 8.7 AND m.rating <= 9.0;
---
+++ session2/result3b.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT p.pid FROM Person p, Acts a WHERE a.pid = p.pid
+AND EXISTS ( 
+SELECT * FROM Movie m  –­ the movies exists and a plays in it
+WHERE m.mid = a.mid 
+AND m.name = 'Back to the Future' 
+) 
+); 
