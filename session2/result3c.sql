a717333c561005d849eb14e0e7b3136c819ce6b7
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 15:27:15 +0200

    first rewriting query. The shunting rule is used.

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,1 +1,1 @@
-SELECT p.name FROM Person p WHERE EXISTS( SELECT * FROM Writes w WHERE w.pid = p.pid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = w.mid);
+SELECT DISTINCT p.name FROM Person p, Writes w WHERE w.pid = p.pid AND NOT EXISTS(SELECT * FROM Directs d WHERE d.mid = w.mid);
