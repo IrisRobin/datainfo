8f667ce6cb4be1718d4d210501ad7090051254e0
Iris Heerlien <i.r.heerlien@student.utwente.nl>
2014-05-06 15:19:27 +0200

    second step writing query

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,1 +1,1 @@
-SELECT p.name FROM person p WHERE 'p is a writer that wrote a movie without director';
+SELECT p.name FROM person p WHERE EXISTS( SELECT * FROM Writes W WHERE w.pid = p.pid AND 'w wrote a movie without director';
